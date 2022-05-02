7661c70e3d60089f110685c8db137307d7537c9e
PedroR18 <poupas18@gmail.com>
2022-05-02 18:23:25 +0100

    .

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,13 +3,53 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
+  technologies = [
+    {
+      name: 'HTML',
+      pic: 'assets/html.webp',
+      skill: 90,
+    },
+    {
+      name: 'CSS',
+      pic: 'assets/css.webp',
+      skill: 90,
+    },
+    {
+      name: 'Sass',
+      pic: 'assets/sass.webp',
+      skill: 90,
+    },
+    {
+      name: 'Javascript',
+      pic: 'assets/javascript.webp',
+      skill: 80,
+    },
+    {
+      name: 'Typescript',
+      pic: 'assets/typescript.webp',
+      skill: 70,
+    },
+    {
+      name: 'React',
+      pic: 'assets/react.webp',
+      skill: 60,
+    },
+    {
+      name: 'Angular',
+      pic: 'assets/angular.webp',
+      skill: 50,
+    },
+    {
+      name: 'NodeJS',
+      pic: 'assets/node.webp',
+      skill: 50,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
