20ad985240be189550188435382d964a8058f8fb
PedroR18 <poupas18@gmail.com>
2022-05-03 17:45:25 +0100

    .

--- src/app/dock/dock.component.ts
+++ src/app/dock/dock.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
@@ -7,23 +7,18 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
   styleUrls: ['./dock.component.scss'],
 })
 export class DockComponent implements OnInit {
-  icons = [
+  iconsFull = [
     'assets/Finder.webp',
     'assets/Launchpad.webp',
     'assets/Safari.webp',
     'assets/Messages.webp',
     'assets/Mail.webp',
-    'assets/Maps.webp',
     'assets/Photos.webp',
     'assets/FaceTime.webp',
     'assets/Calendar.webp',
     'assets/Contacts.webp',
-    'assets/Reminders.webp',
-    'assets/Notes.webp',
     'assets/TV.webp',
     'assets/Music.webp',
-    'assets/Podcasts.webp',
-    'assets/News.webp',
     'assets/AppStore.webp',
     'assets/Settings.webp',
     '',
@@ -31,11 +26,55 @@ export class DockComponent implements OnInit {
     'assets/Trash.webp',
   ];
 
+  iconsMd = [
+    'assets/Finder.webp',
+    'assets/Launchpad.webp',
+    'assets/Safari.webp',
+    'assets/Messages.webp',
+    'assets/Mail.webp',
+    'assets/Photos.webp',
+    'assets/FaceTime.webp',
+    'assets/AppStore.webp',
+    'assets/Settings.webp',
+    '',
+    'assets/Folder.webp',
+    'assets/Trash.webp',
+  ];
+
+  iconsSm = [
+    'assets/Finder.webp',
+    'assets/Safari.webp',
+    'assets/Messages.webp',
+    'assets/Mail.webp',
+    'assets/Photos.webp',
+    'assets/FaceTime.webp',
+    'assets/AppStore.webp',
+    'assets/Settings.webp',
+  ];
+
+  activeIcons =
+    window.innerWidth > 1050
+      ? this.iconsFull
+      : window.innerWidth > 800
+      ? this.iconsMd
+      : this.iconsSm;
+
   drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.icons, event.previousIndex, event.currentIndex);
+    moveItemInArray(this.activeIcons, event.previousIndex, event.currentIndex);
   }
 
   constructor() {}
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    if (window.innerWidth > 1050) {
+      this.activeIcons = this.iconsFull;
+    } else if (window.innerWidth > 800) {
+      this.activeIcons = this.iconsMd;
+    } else if (window.innerWidth > 600) {
+      this.activeIcons = this.iconsSm;
+    }
+  }
 
   ngOnInit(): void {}
 }
